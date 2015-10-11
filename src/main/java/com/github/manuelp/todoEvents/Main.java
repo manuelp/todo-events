6478cf7ce09bc615a124ab8228d81595e138dba6
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-10-11 18:08:59 +0200

    Moved HTTP API adapter to `Server` class
    We want to separate ports/adapters from *behaviours*, and surely from the `Main` entry point.

--- src/main/java/com/github/manuelp/todoEvents/Main.java
+++ src/main/java/com/github/manuelp/todoEvents/Main.java
@@ -1,42 +1,18 @@
 package com.github.manuelp.todoEvents;
 
-import com.github.manuelp.todoEvents.events.TodoCreated;
 import com.github.manuelp.todoEvents.postgresql.RelationalEventStore;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializer;
-import fj.data.List;
+import com.github.manuelp.todoEvents.web.Server;
 import me.manuelp.jevsto.EventStore;
-import me.manuelp.jevsto.dataTypes.Event;
-import spark.Spark;
-
-import static com.github.manuelp.todoEvents.events.Events.TODO_CREATED;
 
 public class Main {
-  private static Gson gson;
 
   public static void main(String[] args) {
     EventStore eventStore = new RelationalEventStore(
         "jdbc:postgresql://localhost:5432/eventstore?user=eventstore&password=eventstore");
     TodoList todoList = new TodoList(eventStore);
 
-    gson = new GsonBuilder().registerTypeAdapter(TodoCreated.class, todoCreatedSerializer()).create();
-    Spark.get("/created", (req, res) -> {
-      res.type("application/json");
-      List<Event> events = eventStore.getAll().filter(e -> Event.isOfType(TODO_CREATED.getType()).call(e));
-      return gson.toJson(events.map(TODO_CREATED.reader()).toJavaList());
-    });
+    new Server().startServer(eventStore);
   }
 
-  private static JsonSerializer<TodoCreated> todoCreatedSerializer() {
-    return (x, t, c) -> {
-      JsonObject data = new JsonObject();
-      data.addProperty("id", x.getId().toString());
-      data.addProperty("created", x.getCreated().format(java.time.format.DateTimeFormatter.ISO_DATE_TIME));
-      data.addProperty("title", x.getTitle());
-      data.addProperty("notes", x.getNotes());
-      return data;
-    };
-  }
+
 }
---
+++ src/main/java/com/github/manuelp/todoEvents/web/Server.java
@@ -0,0 +1,40 @@
+package com.github.manuelp.todoEvents.web;
+
+import com.github.manuelp.todoEvents.events.TodoCreated;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializer;
+import fj.data.List;
+import me.manuelp.jevsto.EventStore;
+import me.manuelp.jevsto.dataTypes.Event;
+
+import static com.github.manuelp.todoEvents.events.Events.TODO_CREATED;
+import static spark.Spark.get;
+
+public class Server {
+  private static Gson gson;
+
+  public Server() {
+    gson = new GsonBuilder().registerTypeAdapter(TodoCreated.class, todoCreatedSerializer()).create();
+  }
+
+  public void startServer(EventStore eventStore) {
+    get("/created", (req, res) -> {
+      res.type("application/json");
+      List<Event> events = eventStore.getAll().filter(e -> Event.isOfType(TODO_CREATED.getType()).call(e));
+      return gson.toJson(events.map(TODO_CREATED.reader()).toJavaList());
+    });
+  }
+
+  private JsonSerializer<TodoCreated> todoCreatedSerializer() {
+    return (x, t, c) -> {
+      JsonObject data = new JsonObject();
+      data.addProperty("id", x.getId().toString());
+      data.addProperty("created", x.getCreated().format(java.time.format.DateTimeFormatter.ISO_DATE_TIME));
+      data.addProperty("title", x.getTitle());
+      data.addProperty("notes", x.getNotes());
+      return data;
+    };
+  }
+}
