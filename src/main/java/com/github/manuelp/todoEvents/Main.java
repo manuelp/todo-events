802e3f79ebc746c32eba873167d29bf69b7fabe5
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-10-11 18:03:41 +0200

    HTTP+JSON API POC
    POC of a REST API using Spark. Also used Gson for JSON (de)serialization.

--- src/main/java/com/github/manuelp/todoEvents/Main.java
+++ src/main/java/com/github/manuelp/todoEvents/Main.java
@@ -1,40 +1,42 @@
 package com.github.manuelp.todoEvents;
 
+import com.github.manuelp.todoEvents.events.TodoCreated;
 import com.github.manuelp.todoEvents.postgresql.RelationalEventStore;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializer;
+import fj.data.List;
 import me.manuelp.jevsto.EventStore;
 import me.manuelp.jevsto.dataTypes.Event;
-import rx.functions.Action1;
+import spark.Spark;
 
 import static com.github.manuelp.todoEvents.events.Events.TODO_CREATED;
 
 public class Main {
+  private static Gson gson;
+
   public static void main(String[] args) {
-    //    EventStore eventStore = new MemoryEventStore();
     EventStore eventStore = new RelationalEventStore(
         "jdbc:postgresql://localhost:5432/eventstore?user=eventstore&password=eventstore");
     TodoList todoList = new TodoList(eventStore);
 
-    //    eventStore.getEvents().subscribeOn(Schedulers.io()).subscribe(printEvent());
-    //    eventStore.getEvents().subscribeOn(Schedulers.io()).filter(e -> e.getType().equals(TODO_CREATED.getType()))
-    //              .subscribe(printDeserializedEvent());
-    //
-    //    Either<Throwable, UUID> res = todoList.addTodo("Test this", "This must be tested thoroughly.");
-    //    System.out.println("Created todo item result: " + res);
-
-    //    Option<Event> res = eventStore.getById(UUID.fromString("2f58d8d2-3e85-44b0-9e7d-46a5687cb9e1"));
-    //    Observable.just(res.some()).subscribe(printDeserializedEvent());
-
-    eventStore.getAllEvents().subscribe(printDeserializedEvent());
-
-    //    eventStore.getAllEventsFrom(LocalDateTime.now()).subscribe(printDeserializedEvent());
+    gson = new GsonBuilder().registerTypeAdapter(TodoCreated.class, todoCreatedSerializer()).create();
+    Spark.get("/created", (req, res) -> {
+      res.type("application/json");
+      List<Event> events = eventStore.getAll().filter(e -> Event.isOfType(TODO_CREATED.getType()).call(e));
+      return gson.toJson(events.map(TODO_CREATED.reader()).toJavaList());
+    });
   }
 
-  private static Action1<Event> printEvent() {
-    return e -> System.out.println(String.format("Serialized -> %s (data size: %d bytes)", e.toString(),
-                                                 e.getData().getData().length));
-  }
-
-  private static Action1<Event> printDeserializedEvent() {
-    return e -> System.out.println(String.format("Deserialized -> %s", TODO_CREATED.reader().f(e)));
+  private static JsonSerializer<TodoCreated> todoCreatedSerializer() {
+    return (x, t, c) -> {
+      JsonObject data = new JsonObject();
+      data.addProperty("id", x.getId().toString());
+      data.addProperty("created", x.getCreated().format(java.time.format.DateTimeFormatter.ISO_DATE_TIME));
+      data.addProperty("title", x.getTitle());
+      data.addProperty("notes", x.getNotes());
+      return data;
+    };
   }
 }
